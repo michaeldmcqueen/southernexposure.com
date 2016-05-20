c49db76594e0db6e42a9137d40c3ecbd6089feb5
Michael McQueen <michaeldmcqueen@gmail.com>
2016-05-20 11:00:46 -0600

    updating html template

--- themes/novel/templates/system/html.tpl.php
+++ themes/novel/templates/system/html.tpl.php
@@ -40,7 +40,14 @@
  * @see template_process()
  */
 ?>
+
 <!DOCTYPE html>
+<!--[if IEMobile 7 ]> <html dir="ltr" lang="en-US"class="no-js iem7"> <![endif]-->
+<!--[if lt IE 7 ]> <html dir="ltr" lang="en-US" class="no-js ie6 oldie"> <![endif]-->
+<!--[if IE 7 ]>    <html dir="ltr" lang="en-US" class="no-js ie7 oldie"> <![endif]-->
+<!--[if IE 8 ]>    <html dir="ltr" lang="en-US" class="no-js ie8 oldie"> <![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html dir="ltr" lang="en-US" class="no-js"><!--<![endif]-->
+
   <head>
     <?php print $head; ?>
     <title><?php print $head_title; ?></title>
